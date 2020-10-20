f836aed78e080a6b08b09716b9695c435fa741f5
Éric Pinto <ericgrandopinto@gmail.com>
2020-10-20 11:56:46 -0300

    adicionado recurso de busca por todos pedidos

--- src/main/java/projeto/cliente/controller/PedidoController.java
+++ src/main/java/projeto/cliente/controller/PedidoController.java
@@ -2,15 +2,13 @@ package projeto.cliente.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 import projeto.cliente.entity.Pedido;
 import projeto.cliente.service.PedidoService;
 
 import java.net.URI;
+import java.util.List;
 
 @RestController
 @RequestMapping(value = "/pedidos")
@@ -19,6 +17,11 @@ public class PedidoController {
 
     @Autowired
     PedidoService pedidoService;
+
+    @GetMapping
+    public List<Pedido> findAll(){
+        return pedidoService.findAll();
+    }
 
     @PostMapping
     public ResponseEntity<Void> insert(@RequestBody Pedido pedido){
--- src/main/java/projeto/cliente/service/PedidoService.java
+++ src/main/java/projeto/cliente/service/PedidoService.java
@@ -5,11 +5,17 @@ import org.springframework.stereotype.Service;
 import projeto.cliente.entity.Pedido;
 import projeto.cliente.repository.PedidoRepository;
 
+import java.util.List;
+
 @Service
 public class PedidoService {
 
     @Autowired
     PedidoRepository pedidoRepository;
+
+    public List<Pedido> findAll(){
+        return pedidoRepository.findAll();
+    }
 
     public Pedido insert(Pedido pedido){
         return pedidoRepository.insert(pedido);
