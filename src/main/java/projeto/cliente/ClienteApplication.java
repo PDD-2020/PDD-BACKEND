8b7a7056befcbfbf39544541da5875e91024d0f5
Felipe Souza <felipesouza.dev@gmail.com>
2020-10-23 20:54:01 -0300

    Testes cors

--- src/main/java/projeto/cliente/ClienteApplication.java
+++ src/main/java/projeto/cliente/ClienteApplication.java
@@ -2,8 +2,10 @@ package projeto.cliente;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 @SpringBootApplication
+@EnableWebMvc
 public class ClienteApplication {
 
     public static void main(String[] args) {
