542d45079d4b228d78a418cbfdca0438aa516bd1
Desirée Menezes <deemenezes@hotmail.com>
2020-11-10 22:03:24 -0300

    Revert "Pdd 5 crud produtos"

--- src/main/java/projeto/cliente/repository/ProdutoRepository.java
+++
@@ -1,9 +0,0 @@
-package projeto.cliente.repository;
-
-import org.springframework.data.mongodb.repository.MongoRepository;
-import org.springframework.stereotype.Repository;
-import projeto.cliente.entity.Produto;
-
-@Repository
-public interface ProdutoRepository extends MongoRepository<Produto, String> {
-}
--- src/main/java/projeto/cliente/service/PedidoService.java
+++ src/main/java/projeto/cliente/service/PedidoService.java
@@ -23,7 +23,7 @@ public class PedidoService {
     }
 
     public Pedido insert(Pedido pedido){
-        return pedidoRepository.save(pedido);
+        return pedidoRepository.insert(pedido);
     }
 
     public void delete(String id){
